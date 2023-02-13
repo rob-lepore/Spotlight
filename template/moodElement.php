4da804e922e2e3158aef4e684237d875fec3cf0c
Roberto <roberto.lepore2@studio.unibo.it>
2023-02-13 21:18:21 +0100

    accessibility fixes

--- template/moodElement.php
+++ template/moodElement.php
@@ -1,5 +1,5 @@
 <div class="<?php echo 'my-2 d-flex align-items-center elevation-1 p-3 gradient-' . $moodData["gradient"] ?>" style="border-radius: 10px">
-    <img class='album-cover' src=<?php echo $moodData["track"]->album->images[0]->url ?>>
+    <img class='album-cover' src=<?php echo $moodData["track"]->album->images[0]->url ?> alt="album cover">
     <div class="overflow-hidden d-block">
         <span><?php echo $moodData["username"] ?> is feeling <?php echo $moodData["emo"] ?></span>
         <br>
--- template/replyElement.php
+++ template/replyElement.php
@@ -1,6 +1,6 @@
 <div class="d-flex my-3 ms-auto" style="width: calc(100% - 3.5rem)">
     <div style="padding-top: 0.4rem;">
-        <img class="profile-pic" src=<?php echo "./upload/" . $commentData["profilePic"] ?> alt="Profile picture not found">
+        <img class="profile-pic" src=<?php echo "./upload/" . $commentData["profilePic"] ?> alt="Profile picture">
     </div>
     <div class="comment surface w-100">
         <span class="label-large"><?php echo $commentData["username"] ?></span>
